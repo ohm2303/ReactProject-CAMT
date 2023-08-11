bdb3d1f6aa0ec7acdf74b3ddac98f21e49cb31b1
Supattha <118960003+Supattha@users.noreply.github.com>
2023-08-12 06:02:32 +0700

    ae

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,15 +1,12 @@
-
 import React, { useState } from "react";
-
 import Search from "./feature/SubComponent/Search";
 import Input from "./feature/SubComponent/Input";
 import Text from "./feature/SubComponent/Text";
-import cross from "./Icon/cross-circle.png"
-import cross1 from "./Icon/cross-circle (1).png"
-
+import cross from "./Icon/cross-circle.png";
+import cross1 from "./Icon/cross-circle (1).png";
 import "./App.css";
 import ButtonIcon from "./feature/SubComponent/ButtonIcon";
-import OptionMain from "./feature/OptionMain";
+import PhotoSlid from "./feature/PhotoSlid";
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
@@ -23,13 +20,17 @@ function App() {
     setInputValue(event.target.value);
   };
 
+  const imageslide = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmOA0CrgQ3eGYvBvQTW9Qad3T8kR-8e2A1S2FGo_eTblluNq8PT62g4Ij5YwSuA98VmNk&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZtBiLGUzd2KZeyWLkmY9fYJD8iIHwy9ZRkwqtGmFFrhX6XZNhmIVXaCibCcVdsKuJcrc&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOFdYghoN17eaN_SnZ1UiozwJKasfEpmCrVrBeLNWw3dhShfFVeAnmm72caVtpJQHIaRQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9VVTVl-kTz0iZ7x8AO-nx-UuHvcQPdk70FET3SnSeABqC-HONLYCvFQjqRkoM7C3mkBU&usqp=CAU",
+    "https://chytomo.com/wp-content/uploads/2022/12/creative-composition-for-world-book-day.jpg",
+  ];
+
   return (
     <div className="App">
-      <Search
-        value={searchValue}
-        onChange={handleSearchChange}
-        size={"small"}
-      />
+      <Search value={searchValue} onChange={handleSearchChange} size={"small"} />
       <Input
         type="text"
         placeholder="Enter something..."
@@ -40,23 +41,19 @@ function App() {
       <Text size={18}>test</Text>
 
       <ButtonIcon
-        
         defaultImg={cross}
         hoveredImg={cross1}
         url={"./feature/Home.js"}
-        title={"cancle"}
-        className = "icon-button"
+        title={"cancel"}
+        className="icon-button"
       />
-
-      <OptionMain />
+        
+      <div className="photoslid-container">
+        <PhotoSlid images={imageslide} />
+        <PhotoSlid images={imageslide} />
+        <PhotoSlid images={imageslide} />
+      </div>
     </div>
-    //test
-
-    //lll
-
-
-    //jane
-//sadsad
   );
 }
 
--- react-project/src/feature/PhotoSlid.js
+++ react-project/src/feature/PhotoSlid.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './PhotoSlid.css'; // Make sure this path is correct based on your project structure
 
-//create photo slid
-function PhotoSlid(){
-    return (
-        <></>
-    );
-}
+const PhotoSlid = ({ images }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-//export
+  const nextSlide = () => {
+    setCurrentSlide(prevSlide => (prevSlide + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(prevSlide => (prevSlide - 1 + images.length) % images.length);
+  };
+
+  
+
+  return (
+    
+    <div className="image-slider" >
+      <button className="slider-button prev-button" onClick={prevSlide}>
+        {'<'}
+      </button>
+      <img src={images[currentSlide]} alt={`Slide ${currentSlide}`} className="slider-image" />
+      <button className="slider-button next-button" onClick={nextSlide}>
+        {'>'}
+      </button>
+    </div>
+  );
+};
+
+PhotoSlid.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
 export default PhotoSlid;
